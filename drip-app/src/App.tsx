225482634dfe2b48dc48888deec63d4e54c6cca5
Alex Jercan <jercan_alex27@yahoo.com>
2023-04-10 12:51:19 +0300

    feat: link app to api

--- drip-app/src/App.tsx
+++ drip-app/src/App.tsx
@@ -1,7 +1,19 @@
 import type { Component } from 'solid-js'
+import { createResource } from 'solid-js'
+
+const fetchMessage = async () => (await fetch('http://localhost:8080')).text()
 
 const App: Component = () => {
-    return <div>Hello, World!</div>
+    const [message] = createResource(fetchMessage)
+
+    return (
+        <>
+            <span>{message.loading && 'Loading...'}</span>
+            <div>
+                <pre>{JSON.stringify(message(), null, 2)}</pre>
+            </div>
+        </>
+    )
 }
 
 export default App
