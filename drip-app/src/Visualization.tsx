1ea5a8de9da1affc299883eb500b6854d2398285
Alex Jercan <jercan_alex27@yahoo.com>
2023-04-12 16:03:11 +0300

    feat: added plotly demo

--- drip-app/src/Visualization.tsx
+++ drip-app/src/Visualization.tsx
@@ -1,4 +1,5 @@
-import { Component } from 'solid-js'
+import { Component, createEffect } from 'solid-js'
+import * as Plotly from 'plotly.js-basic-dist'
 
 type Value = string | number | boolean | null
 
@@ -11,11 +12,20 @@ type Props = {
 }
 
 const Visualization: Component<Props> = (props: Props) => {
-    return (
-        <>
-            <div>{JSON.stringify(props.data)}</div>
-        </>
-    )
+    let plot_div_id = 'plot_div_' + Math.random()
+
+    createEffect(() => {
+        let x = props.data["time"] as string[];
+        let y = props.data["co"] as number[];
+        Plotly.newPlot(
+            plot_div_id,
+            [{ x, y, type: 'scatter' }],
+            { title: 'My Plot' },
+            { responsive: true }
+        )
+    })
+
+    return <div id={plot_div_id} />
 }
 
 export default Visualization
